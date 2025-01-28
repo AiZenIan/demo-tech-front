859b064a8bc887ca64138df498b4e183ddb9e0e7
Ian Love <iannatelove@gmail.com>
2025-01-28 09:30:01 -0700

    example array function

---
+++ api/example.ts
@@ -0,0 +1,8 @@
+
+
+export function quizExample(numberAr : number[]){
+
+    const sortArray = numberAr.sort((a, b) => a - b);
+    return sortArray;
+
+}
--- api/numbers.ts
+++ api/numbers.ts
@@ -1,2 +1,10 @@
+import * as Example from './example'
+
+
 let userName : string = "Princess Peach"
 let age: number = 99
+let numAr = [10, 2, 3, 4, 5, 6, 7, 8, 9, 1]
+
+Example.quizExample(numAr)
+
+
